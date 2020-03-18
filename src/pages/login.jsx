fd0418b15365f800b518c2ea37d483663044fcf5
Mohith Rao <mohith.rao96@gmail.com>
2020-03-17 19:59:37 -0500

    opened frontend

---
+++ src/pages/login.jsx
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { Box, TextField, Grid, Button } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+
+
+
+class Login extends Component{
+
+ // <Route newUserPath='/newuser' component={newUser}/>
+
+  newUserRoute(path){
+    this.props.history.push(path)
+  }
+
+  render(){
+    return (
+      <Box className='login-window'
+        border={1}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+            <TextField className='login-box'
+              placeholder='Username'
+              variant='outlined'
+              fullWidth='true'>
+                placeholder
+            </TextField>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField className='login-box'
+                placeholder='Password'
+                variant='outlined'
+                fullWidth='true'>
+                  placeholder
+              </TextField>
+            </Grid>
+            <Grid item xs={2}></Grid>
+            <Grid item xs={8}>
+              <Button fullWidth='true'
+                variant="contained"
+                color="primary"
+                >
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={2}></Grid>
+            <Grid item xs={6}>
+              <Button variant='text'
+                fullWidth='true'
+                onClick={() => this.newUserRoute('/newuser')}>
+                Create Account
+              </Button>
+            </Grid>
+            <Grid item xs={6}>
+              <Button variant='text'
+                fullWidth='true'>
+                Forgot Password?
+              </Button>
+            </Grid>
+          </Grid>
+      </Box>
+    );
+  }
+};
+
+export default withRouter(Login);
