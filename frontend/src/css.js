068191b4ffef110a2b506100b66a4ba715c30dd6
ShawonC <shawon@puppet.com>
2020-06-01 23:42:45 -0500

    sigh

--- frontend/src/css.js
+++
@@ -1,5 +0,0 @@
-// Helper functions for dealing with CSS class names.
-
-export function evenOddClass(i) { return i % 2 ? 'odd' : 'even'; }
-export function stepIDName(stepNumber) { return `step-${stepNumber}`; }
-
